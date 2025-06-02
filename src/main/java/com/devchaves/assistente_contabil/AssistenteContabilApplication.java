c1bcf1c14050268b0d31133d27e30b027032d05f
chaves <chavesprogrammer@gmail.com>
2025-06-02 09:34:45 -0300

    first commit

---
+++ src/main/java/com/devchaves/assistente_contabil/AssistenteContabilApplication.java
@@ -0,0 +1,13 @@
+package com.devchaves.assistente_contabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenteContabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenteContabilApplication.class, args);
+	}
+
+}
