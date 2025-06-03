7847a3bac55e74c162b2ec7cf1e71950a2c16da2
chaves <chavesprogrammer@gmail.com>
2025-06-03 10:52:33 -0300

    add nfe v4 e InformaçõesDosTécnicos

---
+++ src/main/java/com/devchaves/assistente_contabil/pojo/InformacoesResponsavelTecnico.java
@@ -0,0 +1,26 @@
+package com.devchaves.assistente_contabil.pojo;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import lombok.Data;
+
+@Data
+public class InformacoesResponsavelTecnico {
+
+    @JacksonXmlProperty(localName = "CNPJ")
+    private String cnpj;
+
+    @JacksonXmlProperty(localName = "xContato")
+    private String contato;
+
+    @JacksonXmlProperty(localName = "email")
+    private String email;
+
+    @JacksonXmlProperty(localName = "fone")
+    private String telefone;
+
+    @JacksonXmlProperty(localName = "idCSRT")
+    private String idCSRT;
+
+    @JacksonXmlProperty(localName = "hashCSRT")
+    private String hashCSRT;
+}
