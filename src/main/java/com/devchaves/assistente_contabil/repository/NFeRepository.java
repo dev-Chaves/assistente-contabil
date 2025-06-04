e176c9cfa2b77f9982aeb6b94eeea4c48da7a70a
chaves <chavesprogrammer@gmail.com>
2025-06-04 08:50:12 -0300

    add anotação repository

--- src/main/java/com/devchaves/assistente_contabil/repository/NFeRepository.java
+++ src/main/java/com/devchaves/assistente_contabil/repository/NFeRepository.java
@@ -3,6 +3,8 @@ package com.devchaves.assistente_contabil.repository;
 import com.devchaves.assistente_contabil.nfe.model.NFe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NFeRepository extends MongoRepository<NFe, String> {
 }
