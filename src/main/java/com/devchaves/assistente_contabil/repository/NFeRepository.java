1606a6b9f8b9ca4d2dff892bc35180642d123058
chaves <chavesprogrammer@gmail.com>
2025-06-04 10:58:51 -0300

    removendo pacotes do JPA e dependencias

--- src/main/java/com/devchaves/assistente_contabil/repository/NFeRepository.java
+++ src/main/java/com/devchaves/assistente_contabil/repository/NFeRepository.java
@@ -1,7 +1,6 @@
 package com.devchaves.assistente_contabil.repository;
 
 import com.devchaves.assistente_contabil.nfe.model.NFe;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
